8a2d344292ad7622d27245d0036e431029e1924c
Jan Waś <jan.was@starburstdata.com>
2023-05-18 16:09:39 +0200

    Don't create temporary files for SSL cert

--- pkg/trino/driver/driver.go
+++ pkg/trino/driver/driver.go
@@ -4,7 +4,6 @@ import (
 	"crypto/tls"
 	"database/sql"
 	"net/http"
-	"os"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/trinodb/grafana-trino/pkg/trino/models"
@@ -17,24 +16,10 @@ const DriverName string = "trino"
 // Open registers a new driver with a unique name
 func Open(settings models.TrinoDatasourceSettings) (*sql.DB, error) {
 	skipVerify := false
-	sslCertPath := ""
+	sslCert := ""
 	if settings.Opts.TLS != nil {
 		skipVerify = settings.Opts.TLS.InsecureSkipVerify
-		if settings.Opts.TLS.CACertificate != "" {
-			f, err := os.CreateTemp("", "cert")
-			if err != nil {
-				return nil, err
-			}
-			_, err = f.Write([]byte(settings.Opts.TLS.CACertificate))
-			if errClose := f.Close(); errClose != nil && err == nil {
-				err = errClose
-			}
-			if err != nil {
-				return nil, err
-			}
-			sslCertPath = f.Name()
-			defer os.Remove(f.Name())
-		}
+		sslCert = settings.Opts.TLS.CACertificate
 	}
 	client := &http.Client{
 		Transport: &http.Transport{
@@ -51,7 +36,7 @@ func Open(settings models.TrinoDatasourceSettings) (*sql.DB, error) {
 		ServerURI:        settings.URL.String(),
 		Source:           "grafana",
 		CustomClientName: "grafana",
-		SSLCertPath:      sslCertPath,
+		SSLCert:          sslCert,
 	}
 
 	dsn, err := config.FormatDSN()
